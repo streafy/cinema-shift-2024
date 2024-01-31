fea1db6921e90961c85807202335ab67c513ec42
streafy <streafyhs@gmail.com>
2024-02-01 03:04:54 +0700

    feat: add movie domain entity

---
+++ app/src/main/java/com/streafy/cinemashift2024/domain/entity/Movie.kt
@@ -0,0 +1,11 @@
+package com.streafy.cinemashift2024.domain.entity
+
+data class Movie(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val genres: List<String>,
+    val country: String,
+    val userRating: Float,
+    val releaseYear: Int
+)
