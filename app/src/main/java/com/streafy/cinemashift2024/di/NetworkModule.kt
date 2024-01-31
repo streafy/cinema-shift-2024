7acb05fcbf7619937766cdab8523b007bd9d60d5
streafy <streafyhs@gmail.com>
2024-02-01 03:06:55 +0700

    feat: add network di module

---
+++ app/src/main/java/com/streafy/cinemashift2024/di/NetworkModule.kt
@@ -0,0 +1,36 @@
+package com.streafy.cinemashift2024.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.streafy.cinemashift2024.data.remote.MovieApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://shift-backend.onrender.com/"
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        val contentType = "application/json".toMediaType()
+        val json = Json { ignoreUnknownKeys = true }
+
+        return Retrofit.Builder()
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieApi(retrofit: Retrofit): MovieApi =
+        retrofit.create(MovieApi::class.java)
+}
