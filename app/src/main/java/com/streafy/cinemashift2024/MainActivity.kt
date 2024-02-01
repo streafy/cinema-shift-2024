241c54e87ff95666c82114413722375e52c73985
streafy <streafyhs@gmail.com>
2024-02-01 11:58:52 +0700

    feat: add movie list layout

--- app/src/main/java/com/streafy/cinemashift2024/MainActivity.kt
+++ app/src/main/java/com/streafy/cinemashift2024/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.streafy.cinemashift2024.navigation.AppNavigation
 import com.streafy.cinemashift2024.ui.theme.Cinemashift2024Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/streafy/cinemashift2024/presentation/movielist/MovieListScreen.kt
+++ app/src/main/java/com/streafy/cinemashift2024/presentation/movielist/MovieListScreen.kt
@@ -1,8 +1,93 @@
 package com.streafy.cinemashift2024.presentation.movielist
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.streafy.cinemashift2024.domain.entity.Movie
 
 @Composable
-fun MovieListScreen() {
+fun MovieListScreen(
+    viewModel: MovieListViewModel = hiltViewModel()
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
 
+    when (val stateValue = state) {
+        is MovieListUiState.Content -> Content(movies = stateValue.movies)
+        is MovieListUiState.Error -> Text(text = "Error")
+        MovieListUiState.Initial -> Unit
+        MovieListUiState.Loading -> Loading()
+    }
+}
+
+@Composable
+private fun Content(movies: List<Movie>) {
+    Scaffold(
+        topBar = { TopBar() },
+        bottomBar = {
+            BottomBar()
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            Modifier.padding(paddingValues)
+        ) {
+            items(movies) { movie ->
+                MovieCard(movie = movie)
+            }
+        }
+    }
+
+}
+
+@Composable
+private fun TopBar() {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Афиша",
+            modifier = Modifier.padding(vertical = 12.dp),
+            style = MaterialTheme.typography.headlineLarge
+        )
+    }
+}
+
+@Composable
+private fun BottomBar() {
+    Row(
+        modifier = Modifier.fillMaxWidth().height(58.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "Афиша")
+        Text(text = "Билеты")
+        Text(text = "Профиль")
+    }
+}
+
+@Composable
+private fun Loading() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
 }
