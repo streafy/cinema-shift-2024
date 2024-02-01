04c82d87eb53e1ccaf194ecf54b4cee7c102bd02
streafy <streafyhs@gmail.com>
2024-02-01 11:42:22 +0700

    fix: add original name and img fields to movie entity

--- app/src/main/java/com/streafy/cinemashift2024/domain/entity/Movie.kt
+++ app/src/main/java/com/streafy/cinemashift2024/domain/entity/Movie.kt
@@ -3,9 +3,11 @@ package com.streafy.cinemashift2024.domain.entity
 data class Movie(
     val id: Int,
     val name: String,
+    val originalName: String,
     val description: String,
     val genres: List<String>,
     val country: String,
     val userRating: Float,
-    val releaseYear: Int?
+    val releaseYear: Int?,
+    val img: String
 )
