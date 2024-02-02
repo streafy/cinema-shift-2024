2fc0ecc830efc9e4c3a233412597fe679e23bc5d
streafy <streafyhs@gmail.com>
2024-02-02 08:48:35 +0700

    fix: replace text icons in rating bar with vector images

--- app/src/main/java/com/streafy/cinemashift2024/presentation/shared/cardelements/RatingBar.kt
+++ app/src/main/java/com/streafy/cinemashift2024/presentation/shared/cardelements/RatingBar.kt
@@ -1,17 +1,26 @@
 package com.streafy.cinemashift2024.presentation.shared.cardelements
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.streafy.cinemashift2024.R
 
 @Composable
 fun RatingBar(rating: Int, ratingMax: Int) {
     Row {
         repeat(times = rating) {
-            Text(text = "⭐")
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.star_solid_full),
+                contentDescription = null
+            )
         }
         repeat(times = ratingMax - rating) {
-            Text(text = "☆")
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.star_solid),
+                contentDescription = null
+            )
         }
     }
 }
