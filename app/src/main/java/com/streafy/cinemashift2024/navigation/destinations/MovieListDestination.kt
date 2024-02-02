0a0161a7f736ba22aa028af9f04e2376b236886a
streafy <streafyhs@gmail.com>
2024-02-02 08:01:51 +0700

    feat: add navigation from movie list to movie details

--- app/src/main/java/com/streafy/cinemashift2024/navigation/destinations/MovieListDestination.kt
+++ app/src/main/java/com/streafy/cinemashift2024/navigation/destinations/MovieListDestination.kt
@@ -2,12 +2,15 @@ package com.streafy.cinemashift2024.navigation.destinations
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.streafy.cinemashift2024.domain.entity.Movie
 import com.streafy.cinemashift2024.presentation.movielist.MovieListScreen
 
 const val MOVIE_LIST_ROUTE = "movie_list"
 
-fun NavGraphBuilder.movieList() {
+fun NavGraphBuilder.movieList(
+    onMovieClick: (Movie) -> Unit
+) {
     composable(route = MOVIE_LIST_ROUTE) {
-        MovieListScreen()
+        MovieListScreen(onMovieClick = onMovieClick)
     }
 }
