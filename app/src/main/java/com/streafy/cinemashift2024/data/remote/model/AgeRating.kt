0335d3f69350e52f37f1d834fd255a3fea0c4d17
streafy <streafyhs@gmail.com>
2024-02-02 17:18:22 +0700

    refactor: move movie model classes to movie package

--- app/src/main/java/com/streafy/cinemashift2024/data/remote/model/AgeRating.kt
+++
@@ -1,5 +0,0 @@
-package com.streafy.cinemashift2024.data.remote.model
-
-enum class AgeRating {
-    G, PG, PG13, R, NC17
-}
---
+++ app/src/main/java/com/streafy/cinemashift2024/data/remote/model/movie/AgeRating.kt
@@ -0,0 +1,5 @@
+package com.streafy.cinemashift2024.data.remote.model.movie
+
+enum class AgeRating {
+    G, PG, PG13, R, NC17
+}
